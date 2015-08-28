d02b7edd07697af0e992b43216cd9ac94152f78d
Gavin Mogan <gavin@gavinmogan.com>
2015-08-28 12:41:40 -0700

    initial

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bytes"
+	"container/list"
+	"encoding/json"
+	"log"
+	"os/exec"
+	"strings"
+	"time"
+)
+
+// Stat represents stats to send to graphite
+type Stat struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+	DT    int64  `json:"dt"`
+}
+
+func main() {
+	stats := list.New()
+	var out bytes.Buffer
+
+	cmd := exec.Command("typeperf", "-sc", "1", "processor(_total)\\% processor time")
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	chunks := strings.Split(out.String(), ",")
+	chunks = strings.Split(chunks[2], "\"")
+	stat := Stat{"cpu", chunks[1], time.Now().Unix()}
+	stats.PushBack(stat)
+
+	b, err := json.Marshal(stat)
+	log.Printf("Output: %s", string(b))
+}
