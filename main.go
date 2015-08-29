9d75e6c31751d508b719e897eb7efc12a29908a7
Gavin Mogan <gavin@gavinmogan.com>
2015-08-28 17:51:49 -0700

    Memory and process stats

--- main.go
+++ main.go
@@ -17,13 +17,30 @@ import (
 )
 
 // Win32_Volume - wmic volume where DriveType=3 list brief
+// https://msdn.microsoft.com/en-us/library/aa394515(v=vs.85).aspx
 type Win32_Volume struct {
-	Capacity   int64
-	DriveType  int64
+	Capacity   uint64
+	DriveType  uint32
 	FileSystem string
-	FreeSpace  int64
+	FreeSpace  uint64
 	Label      *string
 	Name       string
+}
+
+// Win32_ComputerSystem - wmic ComputerSystem
+// https://msdn.microsoft.com/en-us/library/aa394102(v=vs.85).aspx
+type Win32_ComputerSystem struct {
+	TotalPhysicalMemory uint64
+}
+
+// Win32_OperatingSystem - blah blah
+// https://msdn.microsoft.com/en-us/library/aa394239(v=vs.85).aspx
+type Win32_OperatingSystem struct {
+	FreePhysicalMemory     uint64
+	FreeVirtualMemory      uint64
+	NumberOfProcesses      uint64
+	TotalVirtualMemorySize uint64
+	TotalVisibleMemorySize uint64
 }
 
 // Stat represents stats to send to graphite
@@ -50,6 +67,35 @@ func schedule(what func(), delay time.Duration) chan bool {
 	return stop
 }
 
+func getOperatingSystemStats() (stats []Stat) {
+	var dst []Win32_OperatingSystem
+
+	q := wmi.CreateQuery(&dst, "")
+	err := wmi.Query(q, &dst)
+	if err != nil {
+		log.Fatal(err)
+	}
+	stats = append(stats, Stat{"mem.physical.free", strconv.FormatUint(dst[0].FreePhysicalMemory, 10), time.Now().Unix()})
+	stats = append(stats, Stat{"mem.virtual.free", strconv.FormatUint(dst[0].FreeVirtualMemory, 10), time.Now().Unix()})
+	stats = append(stats, Stat{"mem.virtual.total", strconv.FormatUint(dst[0].TotalVirtualMemorySize, 10), time.Now().Unix()})
+	stats = append(stats, Stat{"mem.visible.total", strconv.FormatUint(dst[0].TotalVisibleMemorySize, 10), time.Now().Unix()})
+	stats = append(stats, Stat{"processes.total", strconv.FormatUint(dst[0].NumberOfProcesses, 10), time.Now().Unix()})
+
+	return
+}
+
+func getComputerSystemStats() (stats []Stat) {
+	var dst []Win32_ComputerSystem
+
+	q := wmi.CreateQuery(&dst, "")
+	err := wmi.Query(q, &dst)
+	if err != nil {
+		log.Fatal(err)
+	}
+	stats = append(stats, Stat{"mem.physical.total", strconv.FormatUint(dst[0].TotalPhysicalMemory, 10), time.Now().Unix()})
+	return
+}
+
 func getDiskStats() (stats []Stat) {
 	var dst []Win32_Volume
 	nonAlphaNumReg, _ := regexp.Compile("[^A-Za-z0-9]+")
@@ -63,12 +109,9 @@ func getDiskStats() (stats []Stat) {
 		keyPrefix := fmt.Sprintf("disk.%s", nonAlphaNumReg.ReplaceAllLiteralString(v.Name, "_"))
 		percent := int64(100 * (float64(v.FreeSpace) / float64(v.Capacity)))
 
-		b, _ := json.Marshal(v)
-		log.Printf("Disk: %s", string(b))
-
 		stats = append(stats, Stat{fmt.Sprintf("%s.percent_used", keyPrefix), strconv.FormatInt(percent, 10), time.Now().Unix()})
-		stats = append(stats, Stat{fmt.Sprintf("%s.free", keyPrefix), strconv.FormatInt(v.FreeSpace, 10), time.Now().Unix()})
-		stats = append(stats, Stat{fmt.Sprintf("%s.avail", keyPrefix), strconv.FormatInt(v.Capacity, 10), time.Now().Unix()})
+		stats = append(stats, Stat{fmt.Sprintf("%s.free", keyPrefix), strconv.FormatUint(v.FreeSpace, 10), time.Now().Unix()})
+		stats = append(stats, Stat{fmt.Sprintf("%s.avail", keyPrefix), strconv.FormatUint(v.Capacity, 10), time.Now().Unix()})
 	}
 
 	return
@@ -91,6 +134,8 @@ func getCPUStats() (stats []Stat) {
 func getStats() (stats []Stat) {
 	stats = append(stats, getCPUStats()...)
 	stats = append(stats, getDiskStats()...)
+	stats = append(stats, getComputerSystemStats()...)
+	stats = append(stats, getOperatingSystemStats()...)
 	return
 }
 
